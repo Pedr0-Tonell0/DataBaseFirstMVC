fb4145f38ff7e6596aee7c542a53e995e0d87cd1
Pedr0-Tonell0 <54557275+Pedr0-Tonell0@users.noreply.github.com>
2022-01-08 17:45:57 -0300

    Commit realizado en el curso EF de Educacion IT
    first commit

---
+++ DataBaseFirst/Controllers/AutoresController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataBaseFirst.Models;
+namespace DataBaseFirst.Controllers
+{
+    public class AutoresController : Controller
+    {
+        BibliotecaOKEntities db = new BibliotecaOKEntities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var lista = from a in db.Autores
+                        select a;
+            return View("Index", lista.ToList());
+        }
+
+        //metodo get, que envie un objeto autor para que lo complete el usuario
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //clase viewBag sirve para compartir informacion entre controller y view
+            ViewBag.IdPais = new SelectList(db.Paises, "ID", "Nombre");
+            ViewBag.Fecha = DateTime.Now.ToShortDateString();
+            //si quiero setear alguna propiedad por default
+            Autore autor = new Autore();
+            autor.Apellido = "(Ingrese aqui el apellido)";
+            return View("Create", autor);
+        }
+
+        //persiste en la db
+        [HttpPost]
+        public ActionResult Create(Autore autor)
+        {
+            db.Autores.Add(autor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult TraerxID(int id)
+        {
+            var autor = db.Autores.Find(id);
+            if(autor == null)
+            {
+                autor.Apellido = "Autor no encontrado";
+            }
+            return View("Detalle", autor);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var autor = db.Autores.Find(id);
+            if (autor == null)
+            {
+                autor.Apellido = "Autor no encontrado";
+            }
+            ViewBag.IdPais = new SelectList(db.Paises, "ID", "Nombre");
+            return View("Edit", autor);
+        }
+
+        //persiste en la db
+        [HttpPost]
+        public ActionResult Edit(Autore autor)
+        {
+            db.Entry(autor).State = EntityState.Modified;
+            db.SaveChanges();
+            //db.ProcModiAutor(autor.ID, autor.Apellido, autor.Nombre, autor.FechaNacimiento, autor.IdPais);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var autor = db.Autores.Find(id);
+            if (autor == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            ViewBag.Autor = autor.Apellido + " " + autor.Nombre;
+            return View("Delete", autor);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Borrar(int? id)
+        {
+            var autor = db.Autores.Find(id);
+            db.Autores.Remove(autor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DataBaseFirst/Controllers/EscritoresController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataBaseFirst.Models;
+
+namespace DataBaseFirst.Controllers
+{
+    public class EscritoresController : Controller
+    {
+        private BibliotecaOKEntities db = new BibliotecaOKEntities();
+
+        // GET: Escritores
+        public ActionResult Index()
+        {
+            var autores = db.Autores.Include(a => a.Pais);
+            return View(autores.ToList());
+        }
+
+        // GET: Escritores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autore autore = db.Autores.Find(id);
+            if (autore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autore);
+        }
+
+        // GET: Escritores/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdPais = new SelectList(db.Paises, "ID", "Nombre");
+            return View();
+        }
+
+        // POST: Escritores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Apellido,Nombre,FechaNacimiento,IdPais")] Autore autore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autores.Add(autore);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdPais = new SelectList(db.Paises, "ID", "Nombre", autore.IdPais);
+            return View(autore);
+        }
+
+        // GET: Escritores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autore autore = db.Autores.Find(id);
+            if (autore == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdPais = new SelectList(db.Paises, "ID", "Nombre", autore.IdPais);
+            return View(autore);
+        }
+
+        // POST: Escritores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Apellido,Nombre,FechaNacimiento,IdPais")] Autore autore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(autore).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdPais = new SelectList(db.Paises, "ID", "Nombre", autore.IdPais);
+            return View(autore);
+        }
+
+        // GET: Escritores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autore autore = db.Autores.Find(id);
+            if (autore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autore);
+        }
+
+        // POST: Escritores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Autore autore = db.Autores.Find(id);
+            db.Autores.Remove(autore);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
